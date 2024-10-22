77db2840b8a03908cf5115d1a24d25eeadbca7a6
anandSgunti <agunti@mednethealth.com>
2024-10-22 12:55:42 +0100

    Update vite.config.ts

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/react-pdf-highlighter-extended/example-app/",
+  base: "/Pdf_Highlighter_React/example-app/",
   build: {
     target: "esnext",
     outDir: "example-app",
