ba26e2261263784896f327aeca87bd555743a762
anandSgunti <agunti@mednethealth.com>
2024-10-22 13:53:53 +0100

    Update vite.config.ts

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react";
-import { createRequire } from 'module';  // Needed to properly resolve require
 
-const require = createRequire(import.meta.url);
 export default defineConfig({
   base: "/Pdf_Highlighter_React/example-app/",
   build: {
@@ -18,12 +16,6 @@ export default defineConfig({
       target: "esnext",
     },
   },
-  resolve: {
-    alias: {
-      'pdfjs-dist/build/pdf.worker.entry': require.resolve('pdfjs-dist/build/pdf.worker.entry'),
-    },
-  },
-  
   define: {
     APP_VERSION: JSON.stringify(process.env.npm_package_version),
   },
