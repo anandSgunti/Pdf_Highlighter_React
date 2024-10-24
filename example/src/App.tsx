102f71ed7e3c9a4915dceb23c4bc42b46839ff96
anandSgunti <agunti@mednethealth.com>
2024-10-24 13:31:23 +0100

    Update App.tsx removed URL revert

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -180,11 +180,7 @@ const App = () => {
   
   
   }
-  const handleSearch = () => {
 
-      console.log("No text or search term to search.");
-      return;
-    }
   // Handle PDF upload from local machine
   const handlePdfUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
     const uploadedFile = event.target.files?.[0];
@@ -222,7 +218,7 @@ const App = () => {
           setPdfScaleValue={(value) => setPdfScaleValue(value)}
           toggleHighlightPen={() => setHighlightPen(!highlightPen)}
         />
-        <div style={{ padding: '20px', display: 'flex', gap: '15px', alignItems: 'center',justifyContent: 'right' }}>
+        <div style={{ padding: '20px', display: 'flex', gap: '15px', alignItems: 'center' }}>
           <input
             type="file"
             accept="application/pdf"
@@ -236,19 +232,6 @@ const App = () => {
           <label htmlFor="pdf-upload" style={styles.uploadButton}>
             Download
           </label>
-
-          
-        <input
-          type="text"
-          placeholder="Search PDF"
-          style={{ padding: '5px', width: '200px' }}
-        />
-        <button onClick={handleSearch} style={styles.uploadButton}>
-          Search
-        </button>
-      
-  
-        </div>
             
         </div>
         {file && (
